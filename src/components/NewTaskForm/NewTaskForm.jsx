a42ffafd7100a21f6e6e598bb179f0865848bce5
Isa <abazovkjks@gmail.com>
2022-10-29 16:04:32 +0600

    NewTaskForm.jsx

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -1,85 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class NewTaskForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: '',
-      min: null,
-      sec: null,
-    };
-  }
+const NewTaskForm = ({ placeholder, title, addItem }) => {
+  const [value, setValue] = useState('');
+  const [min, setMin] = useState('');
+  const [sec, setSec] = useState('');
 
-  onMinValueChange = (event) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const timer = +min * 60 + +sec;
+    console.log(timer);
+    if (value.trim()) addItem(value, timer);
+    setValue('');
+    setMin('');
+    setSec('');
+  };
+
+  const onSecValueChange = (event) => {
+    const value = event.target.value;
+    if (value < 0) return;
+    setSec(value);
+  };
+
+  const onMinValueChange = (event) => {
     let value = event.target.value;
     console.log(value);
     if (value < 0) return;
-    this.setState({
-      min: value,
-    });
+    setMin(value);
   };
 
-  onSecValueChange = (event) => {
-    const value = event.target.value;
-    if (value < 0) return;
-    this.setState({
-      secValue: value,
-    });
-  };
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+      <form onSubmit={handleSubmit} className="new-todo-form">
+        <label className="new-todo-w">
+          <input
+            className="new-todo"
+            placeholder={placeholder}
+            onChange={(event) => setValue(event.target.value)}
+            value={value}
+          />
+        </label>
 
-  render() {
-    const { placeholder, title, addItem } = this.props;
-    const { value, min, sec } = this.state;
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const timer = +min * 60 + +sec;
-      console.log(timer);
-      if (value.trim()) addItem(value, timer);
-      this.setState({ value: '', min: null, sec: null });
-    };
-    return (
-      <div className="header">
-        <h1>{title}</h1>
-        <form onSubmit={handleSubmit} className="new-todo-form">
-          <label className="new-todo-w">
-            <input
-              className="new-todo"
-              placeholder={placeholder}
-              onChange={(event) => this.setState({ value: event.target.value })}
-              value={value}
-            />
-          </label>
-
-          <label className="new-todo-form__timer-label">
-            <input
-              className="new-todo-form__timer"
-              onChange={this.onMinValueChange}
-              type="number"
-              min={0}
-              placeholder={'MIN'}
-              value={min}
-              required
-            />
-          </label>
-          <span>:</span>
-          <label className="new-todo-form__timer-label">
-            <input
-              className="new-todo-form__timer"
-              onChange={this.onSecValueChange}
-              type="number"
-              min={0}
-              placeholder={'SEC'}
-              value={sec}
-              required
-            />
-          </label>
-          <input type="submit" hidden />
-        </form>
-      </div>
-    );
-  }
-}
+        <label className="new-todo-form__timer-label">
+          <input
+            className="new-todo-form__timer"
+            onChange={onMinValueChange}
+            type="number"
+            min={0}
+            placeholder={'MIN'}
+            value={min}
+            required
+          />
+        </label>
+        <span>:</span>
+        <label className="new-todo-form__timer-label">
+          <input
+            className="new-todo-form__timer"
+            onChange={onSecValueChange}
+            type="number"
+            min={0}
+            placeholder={'SEC'}
+            value={sec}
+            required
+          />
+        </label>
+        <input type="submit" hidden />
+      </form>
+    </div>
+  );
+};
 
 NewTaskForm.propTypes = {
   placeholder: PropTypes.string,
