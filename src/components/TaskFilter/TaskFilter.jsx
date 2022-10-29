572cc5b82a693cc77a611730a945a96127c12eb1
Isa <abazovkjks@gmail.com>
2022-10-29 15:39:01 +0600

    TaskFilter.jsx

--- src/components/TaskFilter/TaskFilter.jsx
+++ src/components/TaskFilter/TaskFilter.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class TaskFilter extends React.Component {
-  render() {
-    const { filter, changeFilter } = this.props;
-    return (
-      <ul className="filters">
-        <li>
-          <button type="button" onClick={() => changeFilter('All')} className={filter === 'All' ? 'selected' : null}>
-            All
-          </button>
-        </li>
-        <li>
-          <button
-            type="button"
-            onClick={() => changeFilter('Active')}
-            className={filter === 'Active' ? 'selected' : null}
-          >
-            Active
-          </button>
-        </li>
-        <li>
-          <button
-            type="button"
-            onClick={() => changeFilter('Completed')}
-            className={filter === 'Completed' ? 'selected' : null}
-          >
-            Completed
-          </button>
-        </li>
-      </ul>
-    );
-  }
-}
-
+const TaskFilter = ({ filter, changeFilter }) => {
+  return (
+    <ul className="filters">
+      <li>
+        <button type="button" onClick={() => changeFilter('All')} className={filter === 'All' ? 'selected' : null}>
+          All
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          onClick={() => changeFilter('Active')}
+          className={filter === 'Active' ? 'selected' : null}
+        >
+          Active
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          onClick={() => changeFilter('Completed')}
+          className={filter === 'Completed' ? 'selected' : null}
+        >
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
+};
 TaskFilter.propTypes = {
   filter: PropTypes.string,
   changeFilter: PropTypes.func.isRequired,
